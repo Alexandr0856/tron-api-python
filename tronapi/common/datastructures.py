4dfd83181ce176ac875efdec4f3c9aabb6f79909
Olexandr Savchuk <ppos19soo@gmail.com>
2023-03-16 18:02:49 +0100

    Update to work with new versions of eth_account

--- tronapi/common/datastructures.py
+++ tronapi/common/datastructures.py
@@ -14,6 +14,10 @@ from tronapi.common.formatters import (
     recursive_map,
 )
 
+from attrdict import (
+    AttrDict,
+)
+
 # Hashable must be immutable:
 # "the implementation of hashable collections requires that a key's hash value is immutable"
 # https://docs.python.org/3/reference/datamodel.html#object.__hash__
@@ -210,3 +214,29 @@ class NamedElementOnion(Mapping):
         if not isinstance(elements, Sequence):
             elements = list(elements)
         return iter(elements)
+
+
+class AttributeDict(AttrDict):
+    """
+    See `AttrDict docs <https://github.com/bcj/AttrDict#attrdict-1>`_
+
+    This class differs only in that it is made immutable. This immutability
+    is **not** a security guarantee. It is only a style-check convenience.
+    """
+    def __setitem__(self, attr, val):
+        raise TypeError(
+            'This data is immutable -- create a copy instead of modifying. '
+            'For example, AttributeDict(old, replace_key=replace_val).'
+        )
+
+    def _repr_pretty_(self, builder, cycle):
+        """
+        Custom pretty output for the IPython console
+        """
+        builder.text(self.__class__.__name__ + "(")
+        if cycle:
+            builder.text("<cycle>")
+        else:
+            builder.pretty(dict(self))
+        builder.text(")")
+
